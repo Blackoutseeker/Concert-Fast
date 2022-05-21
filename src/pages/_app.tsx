c7142e8c1aa642c850fb9b47525ce996c2ab065f
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-21 06:33:12 -0300

    feat: Wrapped Component with AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import type { AppProps } from 'next/app'
+import { AuthProvider } from '@services/authProvider'
 import Head from 'next/head'
 import '@styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <AuthProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   )
 }
 
