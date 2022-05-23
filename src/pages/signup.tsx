4320f461fb40a17af875defca63122d7a8c188bb
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-23 06:28:21 -0300

    feat: Created SignUp page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,50 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { HomeHeader } from '@components/index'
+import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
+import { adminAuth } from '@utils/firebaseAdmin'
+import storage from '@services/storage'
+import { Pages } from '@utils/constants'
+import Styles from '@styles/Home.module.css'
+
+const SignUpPage: NextPage = () => {
+  return (
+    <div className={Styles.pageContainer}>
+      <Head>
+        <title>Concert Fast</title>
+        <meta
+          name="description"
+          content="Faça seu cadastro para ter acesso aos serviços da Concert Fast"
+        />
+      </Head>
+      <HomeHeader />
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  try {
+    const getTokenFromCookie = () => {
+      const token = parseCookies(context).token
+      return token
+    }
+
+    const idToken = storage().getToken(getTokenFromCookie)
+    const isAuthenticated = await adminAuth.verifyIdToken(idToken)
+
+    if (isAuthenticated) {
+      return {
+        props: {},
+        redirect: {
+          destination: Pages.Client
+        }
+      }
+    }
+  } catch (_) {}
+  return {
+    props: {}
+  }
+}
+
+export default SignUpPage
