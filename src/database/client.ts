393bac9b359e0f66b30c1060f01c4e92b990d558
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-23 05:46:25 -0300

    feat: Created getClient method

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,5 +1,12 @@
 import { ref, get } from 'firebase/database'
 import { database } from '@utils/firebaseClient'
+import type { Client } from '@models/index'
+
+/**
+ * Check if the client is in the database
+ * @param {string} id - the client id
+ * @returns {Promise<boolean>} `true` if the client is in the database, otherwise `false`
+ */
 
 export const checkIfClientAlreadyExists = async (
   id: string
@@ -7,4 +14,16 @@ export const checkIfClientAlreadyExists = async (
   const clientReference = ref(database, `/clients/${id}`)
   const client = await (await get(clientReference)).val()
   return client !== null
+}
+
+/**
+ * Get a client from the database
+ * @param id - the client id
+ * @returns {Promise<Client | undefined>} the client if it exists, otherwise `undefined`
+ */
+
+export const getClient = async (id: string): Promise<Client | undefined> => {
+  const clientReference = ref(database, `clients/${id}`)
+  const client: Client | undefined = await (await get(clientReference)).val()
+  return client
 }
