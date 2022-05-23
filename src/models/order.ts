4e428588a51d2e7b2ba8ce2fbd4aeeecabe2a7ce
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-05-22 22:26:20 -0300

    feat: Created Order model

---
+++ src/models/order.ts
@@ -0,0 +1,14 @@
+import type { Client } from './index'
+
+type OrderStatus = 'sent' | 'repairing' | 'dispatched'
+
+export type Order = {
+  id: string
+  client: Client
+  equipment: string
+  description: string
+  budget: number
+  orderDate: string
+  status: OrderStatus
+  dispatchDate: string
+}
