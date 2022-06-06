578d6dabf4e1e3ce59ed4a4a4b31c5244a35d4b3
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-06-05 21:39:20 -0300

    chore: Added ADMIN_ID environment variable

--- next.config.js
+++ next.config.js
@@ -13,6 +13,8 @@ const nextConfig = {
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
 
+    ADMIN_ID: process.env.ADMIN_ID,
+
     TZ: process.env.TZ
   }
 }
