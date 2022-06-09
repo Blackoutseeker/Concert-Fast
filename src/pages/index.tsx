6bade2bc4659947f31431e350f5d965290722164
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-06-08 22:49:06 -0300

    feat: Added GetServerSideProps method to check authentication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { HomeHeader, Banner, HomeSection } from '@components/index'
+import { parseCookies } from 'nookies'
+import { adminAuth } from '@utils/firebaseAdmin'
+import storage from '@services/storage'
+import { Pages } from '@utils/constants'
 import Styles from '@styles/Home.module.css'
 
 const HomePage: NextPage = () => {
@@ -19,6 +23,37 @@ const HomePage: NextPage = () => {
       </Banner>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  try {
+    const getTokenFromCookie = () => {
+      const token = parseCookies(context).token
+      return token
+    }
+
+    const idToken = storage().getToken(getTokenFromCookie)
+    const uid = (await adminAuth.verifyIdToken(idToken)).uid
+    const isAdmin = uid === process.env.ADMIN_ID
+
+    if (isAdmin) {
+      return {
+        props: {},
+        redirect: {
+          destination: Pages.Admin
+        }
+      }
+    }
+    return {
+      props: {},
+      redirect: {
+        destination: Pages.Client
+      }
+    }
+  } catch (_) {}
+  return {
+    props: {}
+  }
 }
 
 export default HomePage
